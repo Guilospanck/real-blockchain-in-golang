8af895efd8014e6abcfe5fc93d18557568b380e8
guilospanck <guilherme.rp2010@hotmail.com>
2021-09-09 18:12:55 -0300

    feat: adding other cli commands version and balances

--- cmd/tbb/main.go
+++ cmd/tbb/main.go
@@ -15,9 +15,16 @@ func main() {
 		},
 	}
 
+	tbbCmd.AddCommand(versionCmd)
+	tbbCmd.AddCommand(balancesCmd())
+
 	err := tbbCmd.Execute()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func incorrectUsageErr() error {
+	return fmt.Errorf("incorrect usage")
 }
